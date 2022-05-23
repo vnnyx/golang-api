1d43e2dd617e1c21be34203e28627570e4908602
Firdaus Putra Kurniyanto <firdaus.putra1007@gmail.com>
2022-05-23 21:04:30 +0700

    add error type & status code

---
+++ model/status_code.go
@@ -0,0 +1,10 @@
+package model
+
+const (
+	BAD_REQUEST     = "Bad Request"
+	UNAUTHORIZATION = "Unauthorization"
+	FORBIDDEN       = "Forbidden"
+	NOT_FOUND       = "Not Found"
+	SERVER_ERROR    = "Server Errors"
+	OK              = "OK"
+)
