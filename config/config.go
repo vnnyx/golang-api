54033ff070b86d77db69bf5df4c9f2a5f9380db3
Firdaus Putra Kurniyanto <firdaus.putra1007@gmail.com>
2022-07-18 19:13:50 +0700

    refactor: change to viper

--- config/config.go
+++ config/config.go
@@ -1,24 +1,35 @@
 package config
 
 import (
-	"github.com/joho/godotenv"
-	"golang-simple-api/exception"
-	"os"
+	"github.com/spf13/viper"
+	"github.com/vnnyx/golang-api/exception"
 )
 
-type Config interface {
-	Get(key string) string
+type Config struct {
+	MysqlHostSlave         string `mapstructure:"MYSQL_HOST_SLAVE"`
+	MysqlPoolMin           int    `mapstructure:"MYSQL_POOL_MIN"`
+	MysqlPoolMax           int    `mapstructure:"MYSQL_POOL_MAX"`
+	MysqlIdleMax           int    `mapstructure:"MYSQL_IDLE_MAX"`
+	MysqlMaxIdleTimeMinute int    `mapstructure:"MYSQL_MAX_IDLE_TIME_MINUTE"`
+	MysqlMaxLifeTimeMinute int    `mapstructure:"MYSQL_MAX_LIFE_TIME_MINUTE"`
+	JWTPublicKey           string `mapstructure:"JWT_PUBLIC_KEY"`
+	JWTSecretKey           string `mapstructure:"JWT_SECRET_KEY"`
+	JWTMinute              int    `mapstructure:"JWT_MINUTE"`
+	RedisHost              string `mapstructure:"REDIS_HOST"`
+	RedisPassword          string `mapstructure:"REDIS_PASSWORD"`
 }
 
-type configImpl struct {
-}
+func NewConfig(path string, configName string) (*Config, error) {
+	config := &Config{}
+	viper.AddConfigPath(path)
+	viper.SetConfigName(configName)
+	viper.SetConfigType("env")
 
-func (config *configImpl) Get(key string) string {
-	return os.Getenv(key)
-}
+	viper.AutomaticEnv()
 
-func New(filenames ...string) Config {
-	err := godotenv.Load(filenames...)
+	err := viper.ReadInConfig()
 	exception.PanicIfNeeded(err)
-	return &configImpl{}
+	err = viper.Unmarshal(&config)
+	exception.PanicIfNeeded(err)
+	return config, err
 }
